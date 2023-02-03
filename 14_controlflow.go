fb037052b3bef8c2e706a6ca8e43dff73a2584d5
codingcrisis <codingcrisis@proton.me>
2023-02-03 23:17:29 +0100

    Small clarifications in control flow

--- 14_controlflow.go
+++ 14_controlflow.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+func def1() {
+	fmt.Println("first")
+	// Defer runs the statment at the end of the function
+	defer fmt.Println("second")
+	fmt.Println("third")
+	fmt.Println("end of fun def1")
+}
+
+func def2() {
+	// Defer is executed in LIFO order
+	// So it's gonna reverse the order here
+	defer fmt.Println("first")
+	defer fmt.Println("second")
+	defer fmt.Println("third")
+	fmt.Println("end of fun def2")
+}	
+
 func main() {
-	fmt.Println("test")
+	def1()
+	def2()
+	fmt.Println("end of main")
 }
