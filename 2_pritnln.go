9352e0f3a2c9e31feb1bcf062c684882d656e0b7
codingcrisis <codingcrisis@proton.me>
2023-02-24 22:58:56 +0100

    Fixing comments

--- 2_pritnln.go
+++ 2_pritnln.go
@@ -6,18 +6,19 @@ import (
 )
 
 func main() {
-	//Just print
+	// Just print
 	fmt.Print("Hello")
 
-	//Print + new line
+	// Print + new line
 	fmt.Println(" World!")
 
-	//Print with formatting
+	// Print with formatting
 	fmt.Printf("Today is %s\n", time.Now().Weekday())
 
-	//Print different formats (with positional args
+	// Print different formats (with positional args)
 	fmt.Printf("Default %[1]v, decimal: %[1]d, hex: %[1]x", 17)
 
-	//Print with named args - not sure if possible?
+	// Try printing with named args
+	// apparently not possible in go, point to Rust ;)
 	//fmt.Printf("My name is [surname], [name] [surname]\n", {name = "James"}, {surname = "Bond"})
 }
